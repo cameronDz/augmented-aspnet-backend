a584f93545de2d34f6998e88ea37e3b3b0c2da07
Cam Dziurgot <camerondziurgot@gmail.com>
2020-03-28 14:58:28 -0400

    set up documentation generator

--- AugmentedAspnetBackend/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ AugmentedAspnetBackend/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -2,6 +2,7 @@
 // package to your project.
 ////#define Handle_PageResultOfT
 
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -33,9 +34,7 @@ namespace AugmentedAspnetBackend.Areas.HelpPage
             Justification = "Part of a URI.")]
         public static void Register(HttpConfiguration config)
         {
-            //// Uncomment the following to use the documentation from XML documentation file.
-            //config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
-
+            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/Documentation.xml")));
             //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
             //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
             //// formats by the available formatters.
